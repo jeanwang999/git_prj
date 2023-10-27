1a51f88e7752bf40c4abd39762d1fac13f88253f
Jean Wang <weizi.wang75@gmail.com>
2023-10-27 15:15:29 +0800

    update dist folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+    <h2>第一次修改</h2>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
